82db2315a92ed8fadcd41103a2baeccbe234958c
saharasingh250-sudo <saharasingh250@gmail.com>
2026-03-21 21:15:24 +0530

    Day 2 if-else logic

---
+++ day2_if_else.py
@@ -0,0 +1,45 @@
+print("hii today we are learning if ,else")
+age =int(input("enter your age:"))
+if age >=18:
+  print("you are eligible to vote")
+else:
+ print("you are not eligible to vote")
+age =int(input("Enter your age:"))
+if age == 18:
+ print("you are exactly 18")
+else:
+ print("you are not 18")
+num = int(input("enter number:"))
+if num != 10:
+ print("Number is not 10")
+else:
+ print("Number is equal to 10")
+ age = int(input("Enter your age"))
+ if age >= 18 and age <= 60:
+  print("you are an adult")
+ else :
+  print("Not in adult range")
+  num = int(input("enter a number:"))
+  if num == 5 or num ==10:
+   print("The number is a special number")
+  else:
+   print("The number is a normal number")
+num = int(input("enter a number"))
+if not num ==0:
+ print("number is not zero")
+else:
+ print("number is zero")
+marks = int(input("Enter a number: "))
+
+if marks >= 7 and marks <= 20:
+    print("You are passed the exam")
+else:
+    print("You are failed")
+
+
+home = int(input("Enter no of houses in the area: "))
+
+if home == 100 or home == 200:
+    print("You belong to special area")
+else:
+    print("You belong to normal area")
